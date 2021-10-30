2aae86df3172cb43795e8845ab61881af74119f3
Bethany Janos <bethanyjanos28@gmail.com>
2021-10-29 21:45:20 -0400

    Get name in a good spot

---
+++ components/Hero.js
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <div className="flex-1 flex-col content-center min-h-screen">
+      <div className="flex-wrap p-4 pt-80 md:px-16">
+        <h1 className="text-5xl sm:text-7xl lg:text-9xl dark:text-white">
+          <span className="block md:inline-block font-bold">Bethany </span>
+          <span className="block md:inline-block font-thin">Janos</span>
+        </h1>
+      </div>
+    </div>
+  )
+};
+
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Container from '../components/Container'
 import DarkModeToggle from '../components/DarkModeToggle'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <DarkModeToggle />
+      <Hero />
     </Container>
   )
 }
