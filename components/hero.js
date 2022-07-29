c62ff3e518fc6c8982e280d515d5a821dfee37c1
bethanyj28 <bethanyjanos28@gmail.com>
2022-07-29 15:26:18 -0400

    Finalize hero

--- components/hero.js
+++ components/hero.js
@@ -1,24 +1,42 @@
 import styled from 'styled-components'
 import data from '../content/info.json'
+import { device } from '../styles/devices'
 
 const HeroContainer = styled.div`
-  display: flex;
-  justify-content: end;
-  align-items: flex-start;
-  flex-direction: column;
-  height: 50vh;
+  display: relative;
+  top: 15vh;
+  padding-top: 25vh;
 `
 
 const StyledHeader = styled.h1`
   font-size: 10vh;
+  margin: 0;
+
+  @media ${device.tablet} {
+    font-size: 15vh;
+  }
+`
+
+const StyledTagline = styled.h2`
+  font-size: 2.5vh;
+  color: var(--secondary-text);
+  margin: 0;
+
+  @media ${device.tablet} {
+    font-size: 5vh;
+  }
 `
 
 const Hero = () => {
   return (
     <HeroContainer>
       <StyledHeader>
-        {data.first_name} {data.last_name}
+        <span>{data.first_name}</span>
       </StyledHeader>
+      <StyledHeader>
+        <span>{data.last_name}</span>
+      </StyledHeader>
+      <StyledTagline>{data.hero.tag_line}</StyledTagline>
     </HeroContainer>
   )
 }
