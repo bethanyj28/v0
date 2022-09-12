7a7e054879f26696c836bf8d8a91e4420677a356
bethanyj28 <bethanyjanos28@gmail.com>
2022-09-11 21:33:07 -0400

    Add some finishing touches

---
+++ components/aboutbody.js
@@ -0,0 +1,32 @@
+const AboutBody = () => {
+  return (
+    <>
+      <p>
+        I have been building backend systems powering web experience since 2018.
+        I started my career at Red Ventures, where I worked on applications that
+        provided support to teams across the company. These projects included a
+        chat application that smartly routed users to live agents, a
+        monetization system for tracking leads on education sites, and a content
+        tagging system to facilitate relating and categorizing content across
+        multiple brands.
+      </p>
+      <br />
+      <p>
+        After four years at Red Ventures, I decided to take the next step in my
+        career and joined GitHub at the beginning of 2022. I currently work in
+        the Actions space and focus on the backend systems that power Actions
+        outputs such as logs and artifacts.
+      </p>
+      <br />
+      <p>
+        I am passionate about making coding as accessible as possible. I see
+        this both in my work at GitHub and my commitment to mentoring. I focus
+        on mentoring students who are just getting started in the industry to
+        pass on necessary technical skills and give them the confidence to
+        believe they do deserve to be here.
+      </p>
+    </>
+  )
+}
+
+export default AboutBody
